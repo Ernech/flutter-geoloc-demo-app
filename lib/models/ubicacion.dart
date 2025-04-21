e1fef6588554e7f2453ff5eda6dd73ce507002d8
Ernech <ernechvm0509@gmail.com>
2025-04-21 11:35:41 -0400

    Metodos para agregar ubicacion y marcas

--- lib/models/ubicacion.dart
+++ lib/models/ubicacion.dart
@@ -1,7 +1,8 @@
 class Ubicacion {
+  int? id;
   String? nombre;
   double? latitud;
   double? longitud;
 
-  Ubicacion({this.nombre, this.latitud, this.longitud});
+  Ubicacion({this.id, this.nombre, this.latitud, this.longitud});
 }
--- lib/services/ubicacion_service.dart
+++ lib/services/ubicacion_service.dart
@@ -1,3 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:geoloc_demo_app/models/ubicacion.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class UbicacionService with ChangeNotifier {}
+class UbicacionService with ChangeNotifier {
+  final List<Ubicacion> _ubicaciones = [];
+  final Set<Marker> _markers = {};
+
+  List<Ubicacion> get ubicaciones => _ubicaciones;
+
+  void addUbicacion(Ubicacion ubicacion) {
+    _ubicaciones.add(ubicacion);
+    notifyListeners();
+  }
+
+  void addMark(LatLng latLng) {
+    _markers.add(Marker(
+      markerId: MarkerId('${_markers.length + 1}'),
+      position: LatLng(latLng.latitude, latLng.longitude),
+      infoWindow: const InfoWindow(title: 'La paz Bolivia'),
+    ));
+    notifyListeners();
+  }
+}
