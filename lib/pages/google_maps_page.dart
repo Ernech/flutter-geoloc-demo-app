25d02ef6ee7420d141248e2cfb446ed0120d1c95
Ernech <ernechvm0509@gmail.com>
2025-04-21 12:05:42 -0400

    Refactoring pagina mapa

--- lib/pages/google_maps_page.dart
+++ lib/pages/google_maps_page.dart
@@ -1,64 +1,35 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:geoloc_demo_app/services/ubicacion_service.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
 
-class MapSamplePage extends StatefulWidget {
-  const MapSamplePage({super.key});
-
-  @override
-  State<MapSamplePage> createState() => MapSamplePageState();
-}
-
-class MapSamplePageState extends State<MapSamplePage> {
-  final Completer<GoogleMapController> _controller =
-      Completer<GoogleMapController>();
-  final Set<Marker> _markers = {};
-
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(
-      -16.5463005,
-      -68.0781334,
-    ),
-    zoom: 14.4746,
-  );
-
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+class GoogleMapPage extends StatelessWidget {
+  const GoogleMapPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final ubicacionService = Provider.of<UbicacionService>(context);
     return Scaffold(
       body: GoogleMap(
         mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(37.42796133580664, -122.085749655962),
+          zoom: 14.4746,
+        ),
         onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
+          ubicacionService.controller.complete(controller);
         },
         onTap: (latLng) {
-          setState(() {
-            _markers.add(Marker(
-              markerId: MarkerId('${_markers.length + 1}'),
-              position: LatLng(latLng.latitude, latLng.longitude),
-              infoWindow: const InfoWindow(title: 'La paz Bolivia'),
-            ));
-          });
+          // setState(() {
+          //   _markers.add(Marker(
+          //     markerId: MarkerId('${_markers.length + 1}'),
+          //     position: LatLng(latLng.latitude, latLng.longitude),
+          //     infoWindow: const InfoWindow(title: 'La paz Bolivia'),
+          //   ));
+          // });
         },
-        markers: _markers,
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
+        //  markers: _markers,
       ),
     );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
--- lib/services/ubicacion_service.dart
+++ lib/services/ubicacion_service.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:geoloc_demo_app/models/ubicacion.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -5,11 +7,15 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 class UbicacionService with ChangeNotifier {
   final List<Ubicacion> _ubicaciones = [];
   final Set<Marker> _markers = {};
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
   int _accion = 0;
 
   int get accion => _accion;
 
   List<Ubicacion> get ubicaciones => _ubicaciones;
+
+  Completer<GoogleMapController> get controller => _controller;
 
   void addUbicacion(Ubicacion ubicacion) {
     _ubicaciones.add(ubicacion);
